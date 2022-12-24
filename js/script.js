a73af70e5f49f0341fb2a28f76c5f41381e3dc18
errorcode <errorcode4460@gmail.com>
2022-12-24 08:51:50 -0800

    Data for Portfolio

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let js="Amazing";
+if(js==="Amazing") console.log("Alert")
+
+var typed =new Typed(".typing",{
+    strings:["","a web developer.","a system admin.","a programmer."],
+    typeSpeed:150,
+    BackSpeed:150,
+    loop:true
+})
+const now=2022;
+// let myAge=23;
+name="Htun Htun Win"
+console.log(`My Age: ${now - 2000}`);
+console.log(name+" "+" is a noob coder.")
